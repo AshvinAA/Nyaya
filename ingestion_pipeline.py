cced05228869d78a1086a0da8a7e900ac959100f
AshvinAA <ashvin.afroz@gmail.com>
2026-09-09 19:06:08 +0600

    Add data ingestion pipeline setup

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -7,7 +7,7 @@ from langchain_community.document_loaders import TextLoader,DirectoryLoader
 from langchain_text_splitters import CharacterTextSplitter
 
 #Embedding Model
-from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 #vector db
 from langchain_chroma import Chroma
@@ -47,6 +47,27 @@ def load_documents(docs_path="docs"):
 
     return documents
 
+def create_vector_store(chunks, persist_directory="db/chroma_db"):
+    """Create and persist ChromaDB vector store"""
+    print("Creating embeddings and storing in ChromaDB...")
+        
+    embedding_model = HuggingFaceEmbeddings(
+        model_name="all-MiniLM-L6-v2"
+    )
+    
+    # Create ChromaDB vector store
+    print("--- Creating vector store ---")
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory=persist_directory, 
+        collection_metadata={"hnsw:space": "cosine"} #comparison of best chunks
+    )
+    print("--- Finished creating vector store ---")
+    
+    print(f"Vector store created and saved to {persist_directory}")
+    return vectorstore
+
 def split_documents(documents, chunk_size=1000, chunk_overlap=0):
     """Split documents into smaller chunks with overlap"""
     print("Splitting documents into chunks...")
@@ -79,6 +100,8 @@ def main():
 
     documents = load_documents(docs_path= "docs")
     chunks = split_documents(documents)
+    vectorstore = create_vector_store(chunks)
+
     pass
 
 
